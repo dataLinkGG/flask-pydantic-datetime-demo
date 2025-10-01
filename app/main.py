e1848bcb3d0717f64382d0accb6bd11d70b6290c
Thomas Cionek <thomascionek97@gmail.com>
2025-10-01 20:08:31 -0300

    Format using black

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from flask.json.provider import DefaultJSONProvider
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class Task(BaseModel):
     id: int
     name: str
     execute_at: datetime
+
 
 class PydanticJSONProvider(DefaultJSONProvider):
     def default(self, obj):
@@ -16,12 +18,15 @@ class PydanticJSONProvider(DefaultJSONProvider):
             return obj.isoformat()
         return super().default(obj)
 
+
 app = Flask(__name__)
 app.json = PydanticJSONProvider(app)
+
 
 @app.get("/task")
 def get_task():
     return jsonify(Task(id=1, name="demo", execute_at=datetime.utcnow()))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
